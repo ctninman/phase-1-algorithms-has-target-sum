d27aaf068cb7a842898026eaac49738476a38d0f
Chris Ninman <ctsnackpack@gmail.com>
2021-11-09 14:41:59 -0500

    commit tree 36c6af6a26183c9f2a21798cfcf046996f5a815b

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+// function hasTargetSumIan (array, target) {
+//   const seenNumbers = {};
+//   for (let i = 0; i < array.length; i++) {
+//     const complement = target - array[i];
+//     if (seenNumbers[complement]) return true;
+//     seenNumbers[array[i]] = true;
+//   }
+//    return false;
+// }
+
+// function hasTargetSumIan (array, target) {
+//   const seenNumbers = {};
+//   for (const number of array) {
+//     const complement = target - array;
+//     if (complement in seenNumbers) return true;
+//     seenNumbers[number] = true;
+//   }
+//    return false;
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  if (array.length < 2) {
+    return false
+  }
+  for (let indexOne = 0; indexOne < array.length - 1; indexOne++) {
+     for (let indexTwo = indexOne + 1; indexTwo < array.length; indexTwo++) {
+      let sum = array[indexOne] + array[indexTwo];
+      if (sum === target) {
+        return true;
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Quadratic time: O(n²)
+  This function runs a nested loop
 */
 
 /* 
@@ -12,6 +45,7 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+  For each index of the array, create another index of the array
 */
 
 // You can run `node index.js` to view these console logs
